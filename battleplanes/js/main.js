93efbc071b44aa6fd98f887a2d9c657cf5a78501
Florin Maris <florin.maris@ppb.com>
2023-04-20 21:21:38 +0300

    add stop button

--- battleplanes/js/main.js
+++ battleplanes/js/main.js
@@ -1,15 +1,42 @@
 // Define constants
 const GRID_SIZE = 10;
 const PLANE_PARTS = [
-    { type: 'wing', cells: [{ x: 0, y: 0 }, { x: 1, y: 0 }, { x: 2, y: 0 }, { x: 3, y: 0 }, { x: 4, y: 0 }] },
-    { type: 'wing', cells: [{ x: 0, y: 1 }, { x: 1, y: 1 }, { x: 2, y: 1 }, { x: 3, y: 1 }, { x: 4, y: 1 }] },
-    { type: 'body', cells: [{ x: 5, y: 1 }] },
-    { type: 'tail', cells: [{ x: 8, y: 1 }, { x: 9, y: 1 }, { x: 9, y: 2 }] },
+    {
+        type: "wing",
+        cells: [
+            { x: 0, y: 0 },
+            { x: 1, y: 0 },
+            { x: 2, y: 0 },
+            { x: 3, y: 0 },
+            { x: 4, y: 0 },
+        ],
+    },
+    {
+        type: "wing",
+        cells: [
+            { x: 0, y: 1 },
+            { x: 1, y: 1 },
+            { x: 2, y: 1 },
+            { x: 3, y: 1 },
+            { x: 4, y: 1 },
+        ],
+    },
+    { type: "body", cells: [{ x: 5, y: 1 }] },
+    {
+        type: "tail",
+        cells: [
+            { x: 8, y: 1 },
+            { x: 9, y: 1 },
+            { x: 9, y: 2 },
+        ],
+    },
 ];
 
 // Define variables
 let planeCells = [];
-let timeLeft = 300;
+const TIME_LIMIT = 300;
+let timeLeft = TIME_LIMIT;
+let timer;
 
 // Define functions
 function getRandomInt(max) {
@@ -17,16 +44,44 @@ function getRandomInt(max) {
 }
 
 function createGrid() {
-    const grid = document.querySelector('.grid');
+    const grid = document.querySelector(".grid");
     for (let i = 0; i < 100; i++) {
-        const cell = document.createElement('div');
-        cell.setAttribute('data-id', i);
+        const cell = document.createElement("div");
+        cell.setAttribute("data-id", i);
         grid.appendChild(cell);
     }
 }
+
 function startGame() {
-    document.querySelector(".grid").style.display = "grid";
+    // show the grid
+    const grid = document.querySelector(".grid");
+    grid.style.display = "grid";
+
+    // change the button text and functionality
+    const button = document.querySelector(".start-button");
+    button.innerText = "Stop Game";
+    button.removeEventListener("click", startGame);
+    button.addEventListener("click", stopGame);
+
+    // start the timer
+    timeLeft = TIME_LIMIT;
+    timer = setInterval(countdown, 1000);
 }
 
-document.querySelector(".start-button").addEventListener('click', startGame);
+function stopGame() {
+    // hide the grid
+    const grid = document.querySelector(".grid");
+    grid.style.display = "none";
+
+    // change the button text and functionality
+    const button = document.querySelector(".start-button");
+    button.innerText = "Start Game";
+    button.removeEventListener("click", stopGame);
+    button.addEventListener("click", startGame);
+
+    // stop the timer
+    clearInterval(timer);
+}
+
+document.querySelector(".start-button").addEventListener("click", startGame);
 createGrid();
